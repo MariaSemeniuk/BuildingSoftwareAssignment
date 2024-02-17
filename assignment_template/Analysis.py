b2823c504e8c48dcf62117e2d573f824eca1ccc0
Meryl-Fang <mfang@wellesley.edu>
2024-02-17 09:46:53 -0500

    add compute_analysis and notify_done

--- assignment_template/Analysis.py
+++ assignment_template/Analysis.py
@@ -2,6 +2,7 @@ from typing import Any, Optional
 import matplotlib.pyplot as plt
 import yaml
 import requests
+import numpy as np
 
 
 class Analysis():
@@ -27,11 +28,45 @@ class Analysis():
         print(self.config['figure_title'])
 
     def compute_analysis(self) -> Any:
-        pass
+        '''Analyze previously-loaded data.
+
+        This function compute the mean of pokemons' base experience 
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        analysis_output : Float
+
+        '''
+        return np.mean(self.dataset["base_experience"])
 
     def plot_data(self, save_path: Optional[str] = None) -> plt.Figure:
         pass
 
     def notify_done(self, message: str) -> None:
-        pass
+        ''' Notify the user that analysis is complete.
+
+        Send a notification to the user through the ntfy.sh webpush service.
+
+        Parameters
+        ----------
+        message : str
+        Text of the notification to send
+
+        Returns
+        -------
+        None
+
+        '''
+        topic = 'building-software-assignemt-analysis'
+        message = message
+
+        # send a message through ntfy.sh
+        requests.post(
+            'https://ntfy.sh/' + topic,
+            data=message.encode('utf-8'),
+        )
     
